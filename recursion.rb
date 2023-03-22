f94ea60823736e56f28f40c885b6529682c8b7c7
Matt Pierson <matt.pierson789@gmail.com>
2023-03-22 12:03:01 -0400

    initial set-up

---
+++ recursion.rb
@@ -0,0 +1,11 @@
+
+
+#Range 
+
+# Write a recursive method, range, that takes a start and an end and returns an array of all numbers in that range, 
+# exclusive. For example, range(1, 5) should return [1, 2, 3, 4]. If end < start, you can return an empty array.
+# Write both a recursive and iterative version of sum of an array.
+
+def range 
+
+end 
